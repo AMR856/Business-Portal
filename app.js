de9e5bb4f994ad94fa76c6e58323fa62d89128e0
ameralnas <amer.live477@gmail.com>
2024-07-11 17:44:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+const port = process.env.PORT || 3000;
+dotenv.config('./.env');
+
+console.log(process.env.USER_NAME);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
